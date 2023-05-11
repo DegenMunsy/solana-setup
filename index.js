52395d6f78f9dcfdede204769c4500b30179eba8
Degen Munsy <munner7@gmail.com>
2023-05-11 15:08:41 -0400

    options added

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 const { exec } = require('child_process');
 
-// Function to execute a shell script
-function runScript(scriptPath, callback) {
-    const process = exec(`bash ${scriptPath}`);
+function runCommand(command, callback) {
+    const process = exec(command);
 
     process.stdout.on('data', function(data) {
         console.log(data);
@@ -19,18 +18,49 @@ function runScript(scriptPath, callback) {
     });
 }
 
-// Get command line arguments
+const updateCommands = {
+    'rust': 'rustup update',
+    'solana': 'solana-install update',
+    'anchor': 'avm install latest && avm use latest',
+    'yarn': 'yarn set version stable',
+    'typescript': 'npm install -g typescript@latest'
+};
+
+const installCommands = {
+    'solana': 'sh -c "$(curl -sSfL https://release.solana.com/v1.15.2/install)"',
+    'rust': 'curl --proto \'=https\' --tlsv1.2 -sSf https://sh.rustup.rs | sh',
+    'anchor': 'cargo install --git https://github.com/coral-xyz/anchor avm --locked --force',
+    'anchor-dependencies': 'sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y pkg-config build-essential libudev-dev libssl-dev',
+    'yarn': 'corepack enable && npm i -g corepack && corepack prepare yarn@stable --activate && corepack prepare yarn@3.5.1 --activate',
+    'typescript': 'npm install -g typescript'
+};
+
 const args = process.argv.slice(2);
 
-// Run the appropriate script based on the command line argument
 if (args[0] === 'install') {
-    runScript('./install-all.sh', function() {
-        console.log('Installation complete');
+    args.slice(1).forEach(arg => {
+        const command = installCommands[arg];
+
+        if (command) {
+            runCommand(command, function() {
+                console.log(`${arg} install complete`);
+            });
+        } else {
+            console.log(`Invalid program: ${arg}. Available programs: ${Object.keys(installCommands).join(', ')}`);
+        }
     });
 } else if (args[0] === 'update') {
-    runScript('./update-all.sh', function() {
-        console.log('Update complete');
+    args.slice(1).forEach(arg => {
+        const command = updateCommands[arg];
+
+        if (command) {
+            runCommand(command, function() {
+                console.log(`${arg} update complete`);
+            });
+        } else {
+            console.log(`Invalid program: ${arg}. Available programs: ${Object.keys(updateCommands).join(', ')}`);
+        }
     });
 } else {
-    console.log('Invalid command. Use "install" or "update".');
+    console.log('Invalid command. Use "update".');
 }
